e7a3bbc01b458e42921287cc8d9876237a225b1a
LochanaLithira <lithira505@gmail.com>
2026-01-16 01:46:25 +0530

    Add configuration files and initial data ingestion notebook for project setup

---
+++ src/constants/__init__.py
@@ -0,0 +1,7 @@
+#Import necessary modules
+from pathlib import Path
+
+# Define file paths for configuration, parameters, and schema 
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
+SCHEMA_FILE_PATH = Path("schema.yaml")
