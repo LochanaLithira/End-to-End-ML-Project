ff87452aa1db6aea7803d20cca158b8099a1d6ed
LochanaLithira <lithira505@gmail.com>
2026-01-16 13:51:31 +0530

    Done implementing the data ingestion part

--- src/utils/common.py
+++ src/utils/common.py
@@ -24,16 +24,14 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         raise e
     
 #Function to create directories if they do not exist
-@ensure_annotations
-def create_directories(path_to_directories: list[Path], verbose=True) -> None:
+def create_directories(path_to_directories: list, verbose=True) -> None:
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
         if verbose:
             logger.info(f"Directory created at: {path}")
 
 #Function to create a JSON file from a dictionary
-@ensure_annotations
-def create_json(path: Path, data: dict[str, Any]) -> None:
+def create_json(path: Path, data: dict) -> None:
     with open(path, 'w') as json_file:
         json.dump(data, json_file, indent=4)
     logger.info(f"JSON file created at: {path}")
@@ -47,14 +45,18 @@ def load_json(path: Path) -> ConfigBox:
     return ConfigBox(content)
 
 #Function to save data in binary format using joblib
-@ensure_annotations
 def save_bin(path: Path, data:Any) -> None:
     joblib.dump(value = data, filename = path)
     logger.info(f"Binary file saved at: {path}")
 
 #Function to load data from a binary file using joblib
-@ensure_annotations
 def load_bin(path: Path) -> Any:
     data = joblib.load(filename = path)
     logger.info(f"Binary file loaded from: {path}")
     return data
+
+#Function to get the size of a file in KB
+@ensure_annotations
+def get_size(path: Path) -> str:
+    size_in_kb = round(os.path.getsize(path) / 1024, 2)
+    return f"{size_in_kb} KB"
